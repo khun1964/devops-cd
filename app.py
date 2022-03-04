cf2bbce10f987c9a118b6520e4f3aca821d2a29e
Benoît <khun1964@gmail.com>
2022-03-04 10:55:16 +0100

    fix(ci): correction du yml

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
-from flask import Flask 
+# from flask import Flask 
+# import os
+
+# # Récupération des ENVs
+
+# PORT = os.environ.get("PORT", "80")
+# HOST = os.environ.get("HOST", "0.0.0.0")
+
+# app = Flask("demo")
+
+# @app.get("/")
+# def hello():
+    
+#      """ Fonction appelé pour le chemin par défaut.
+     
+#      Returns :
+#         str: un message de la plus grande importance.
+#      """   
+    
+#     return "hello world !"
+
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=80)
+    
+    
+    """_summary_
+    projet de demo pour la création d'un CD
+
+    Returns:
+        _type_: _description_
+    """
+
+from flask import Flask
 import os
 
-# Récupération des ENVs
-
+# Recuperation des ENVs
 PORT = os.environ.get("PORT", "80")
 HOST = os.environ.get("HOST", "0.0.0.0")
 
@@ -11,18 +42,16 @@ app = Flask("demo")
 @app.get("/")
 def hello():
     
-     """ Fonction appelé pour le chemin par défaut.
-     
-     Returns :
-        str: un message de la plus grande importance.
-     """   
-    
-    return "hello world !"
+    """
+    Fonction appelee pour le chemin par defaut.
+    Returns:
+        str: un message :
+    """
+    return "hello world!"
+
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80)
-    
-    
+    app.run(host="HOST", port=PORT)
 
 
     
